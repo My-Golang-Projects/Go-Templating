a6bfb967c202deb2324bfd57bc4dd6c8d99485d9
shrish <adhikari.shrish1@gmail.com>
2024-10-17 23:12:13 +0545

    Added basic template implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module my_template_prac
+
+go 1.22.0
---
+++ src/pets.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Pet struct {
+	Name   string
+	Sex    string
+	Age    string
+	Intact bool
+	Breed  string
+}
+
+func main() {
+	dogs := []Pet{
+		{Name: "Max", Sex: "Male", Age: "5", Intact: true, Breed: "Golden Retriever"},
+		{Name: "Bella", Sex: "Female", Age: "3", Intact: false, Breed: "Labrador Retriever"},
+		{Name: "Charlie", Sex: "Male", Age: "7", Intact: true, Breed: "German Shepherd"},
+		{Name: "Lucy", Sex: "Female", Age: "2", Intact: false, Breed: "Beagle"},
+		{Name: "Buddy", Sex: "Male", Age: "4", Intact: true, Breed: "Poodle"},
+		{Name: "Molly", Sex: "Female", Age: "6", Intact: false, Breed: "Shih Tzu"},
+		{Name: "Bailey", Sex: "Male", Age: "8", Intact: true, Breed: "Rottweiler"},
+		{Name: "Sadie", Sex: "Female", Age: "1", Intact: false, Breed: "Chihuahua"},
+	}
+
+	var tmpfile = "src/pets.tmpl"
+
+	// create a new template with the name of the file
+	// then parse the file
+	tmpl, err := template.New(tmpfile).ParseFiles(tmpfile)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Execute to print the finished report to the terminal, and also passing in our dogs slice
+	err = tmpl.Execute(os.Stdout, dogs)
+	if err != nil {
+		panic(err)
+	}
+}
