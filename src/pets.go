26419664f9abc0f6228d73db800aede5b397ce3d
shrish <adhikari.shrish1@gmail.com>
2024-10-19 09:35:22 +0545

    Improved templates

--- src/pets.go
+++ src/pets.go
@@ -12,38 +12,39 @@ type Pet struct {
 	Sex    string
 	Age    string
 	Intact bool
-	Breed  string
+	Breed  []string
 }
 
 func main() {
 	dogs := []Pet{
-		{Name: "Max", Sex: "Male", Age: "5", Intact: true, Breed: "Golden Retriever"},
-		{Name: "Bella", Sex: "Female", Age: "3", Intact: false, Breed: "Labrador Retriever"},
-		{Name: "Charlie", Sex: "Male", Age: "7", Intact: true, Breed: "German Shepherd"},
-		{Name: "Lucy", Sex: "Female", Age: "2", Intact: false, Breed: "Beagle"},
-		{Name: "Buddy", Sex: "Male", Age: "4", Intact: true, Breed: "Poodle"},
-		{Name: "Molly", Sex: "Female", Age: "6", Intact: false, Breed: "Shih Tzu"},
-		{Name: "Bailey", Sex: "Male", Age: "8", Intact: true, Breed: "Rottweiler"},
-		{Name: "Sadie", Sex: "Female", Age: "1", Intact: false, Breed: "Chihuahua"},
+		{Name: "Max", Sex: "Male", Age: "5", Intact: true, Breed: []string{"Golden Retriever"}},
+		{Name: "Bella", Sex: "Female", Age: "3", Intact: false, Breed: []string{"Labrador Retriever"}},
+		{Name: "Charlie", Sex: "Male", Age: "7", Intact: true, Breed: []string{"German Shepherd"}},
+		{Name: "Lucy", Sex: "Female", Age: "2", Intact: false, Breed: []string{"Beagle"}},
+		{Name: "Buddy", Sex: "Male", Age: "4", Intact: true, Breed: []string{"Poodle"}},
+		{Name: "Molly", Sex: "Female", Age: "6", Intact: false, Breed: []string{"Shih Tzu"}},
+		{Name: "Bailey", Sex: "Male", Age: "8", Intact: true, Breed: []string{"Rottweiler"}},
+		{Name: "Sadie", Sex: "Female", Age: "1", Intact: false, Breed: []string{"Chihuahua"}},
 		{
 			Name:   "Jujube",
 			Sex:    "Female",
 			Intact: false,
 			Age:    "10 months",
-			Breed:  "German Shepherd/Pitbull",
+			Breed:  []string{"German Shepherd", "Pitbull"},
 		},
 		{
 			Name:   "Zephyr",
 			Sex:    "Male",
 			Intact: true,
 			Age:    "13 years, 3 months",
-			Breed:  "German Shepherd/Border Collie",
+			Breed:  []string{"German Shepherd", "Border Collie"},
 		},
 	}
 
 	funcMap := template.FuncMap{
 		"dec":     func(i, j int) int { return i - j },
 		"replace": strings.ReplaceAll,
+		"join": strings.Join,
 	}
 
 	var tmpfile = "src/pets.tmpl"
